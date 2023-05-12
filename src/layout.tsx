968144db5ccbd4d1a6c1eead0058af19b876c225
elvis-ndubuisi <provictor.ie@gmail.com>
2023-05-12 20:53:44 +0100

    fixes: scroll restoration

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
+import { ScrollRestoration } from "react-router-dom";
 
 function Layout() {
   return (
     <>
       <Outlet />
       <Footer />
+      <ScrollRestoration />
     </>
   );
 }
